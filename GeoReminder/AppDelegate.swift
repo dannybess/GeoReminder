a6c733ecb6e37e562d1aa0de47803ca0731e5e7b
Dhruv Shah <dhruv.shah@gmail.com>
2017-07-29 15:44:27 -0700

    Patrick's Firebase init.

--- GeoReminder/AppDelegate.swift
+++ GeoReminder/AppDelegate.swift
@@ -1,9 +1,9 @@
 //
 //  AppDelegate.swift
-//  GeoReminder
+//  GeoPin
 //
-//  Created by Dhruv Shah on 7/29/17.
-//  Copyright © 2017 Dhruv Shah. All rights reserved.
+//  Created by Patrick Li on 7/29/17.
+//  Copyright © 2017 Dali Labs, Inc. All rights reserved.
 //
 
 import UIKit
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        //FirebaseApp.configure()
+        FirebaseApp.configure()
         return true
     }
 
--- GeoReminder/ViewController.swift
+++ GeoReminder/ViewController.swift
@@ -1,25 +1,68 @@
 //
 //  ViewController.swift
-//  GeoReminder
+//  GeoPin
 //
-//  Created by Dhruv Shah on 7/29/17.
-//  Copyright © 2017 Dhruv Shah. All rights reserved.
+//  Created by Patrick Li on 7/29/17.
+//  Copyright © 2017 Dali Labs, Inc. All rights reserved.
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    //map
+    var myPins = [CLLocation]()
+    var userID = "jdinlkj392djf"
+
+    //firebase
+    var ref: DatabaseReference!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        SatoriWrapper.testMethod()
+
+        //firebase
+        ref = Database.database().reference()
+        ref.child("Users").child(userID).child("GeoLocations").childByAutoId().setValue("d")
+
+    }
+
+    func setPinToLocation(location: CLLocation){
+        var locationX = 1
+        var locationY = 1
+        ref.child("Users").child(userID).child("GeoLocations").childByAutoId()//.child("positionX").setValue(locationX)
+        ref.child("Users").child(userID).child("GeoLocations").childByAutoId()//.child("positionY").setValue(locationX)
+
+    }
+
+    func getMyPins(){
+
+    }
+
+    func getNearbyBounties(){
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
