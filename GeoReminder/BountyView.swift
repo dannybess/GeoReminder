211db5ee09bc0bb1441454f26c39298f8e2ce1e3
liPatrick <patrick8289@gmail.com>
2017-07-30 03:24:37 -0700

    Patrick

---
+++ GeoReminder/BountyView.swift
@@ -0,0 +1,45 @@
+//
+//  BountyView.swift
+//  GeoReminder
+//
+//  Created by Patrick Li on 7/30/17.
+//  Copyright © 2017 Dhruv Shah. All rights reserved.
+//
+
+import UIKit
+
+
+
+class BountyView: UIViewController {
+
+
+
+    var id: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+
+
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
